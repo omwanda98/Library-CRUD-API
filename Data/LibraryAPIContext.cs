122bf09d278682ef4a46cb417a5d878f513cd8df
omwanda98 <omwandajeyy@gmai.com>
2024-10-05 06:49:38 +0300

    first commit

---
+++ Data/LibraryAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Data
+{
+    public class LibraryAPIContext : DbContext
+    {
+        public LibraryAPIContext (DbContextOptions<LibraryAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LibraryAPI.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? author {  get; set; }
+
+    }
+}
